5d1e6d4bb773139199c1cde4c08aa9fa000d22e8
Buğra <librouse98@gmail.com>
2023-10-09 23:45:44 +0300

    v3.2.1
    - Clock fix

--- src/components/header/clock.tsx
+++ src/components/header/clock.tsx
@@ -4,11 +4,17 @@ import { useState } from "react";
 export default function Clock() {
   const [time, setTime] = useState<string>("**:**:**");
 
-  setInterval(() => setTime(dayjs().format("hh:mm:ss")), 1000);
+  setInterval(() => setTime(dayjs().format("HH:mm:ss")), 1000);
 
   return (
-    <div className={`hidden lg:block col-span-2 text-center lg:text-5xl xl:text-6xl  2xl:text-8xl font-bold text-light transition`}>
-      {`${time}`}
+    <div
+      className={`col-span-2 hidden items-center justify-center text-center font-bold text-light transition lg:flex lg:text-4xl xl:text-5xl 2xl:text-7xl`}
+    >
+      <div className={`lg:w-14 xl:w-20 2xl:w-24`}>{`${time.slice(0, 2)}`}</div>
+      <div className={``}>:</div>
+      <div className={`lg:w-14 xl:w-20 2xl:w-24`}>{`${time.slice(3, 5)}`}</div>
+      <div className={``}>:</div>
+      <div className={`lg:w-14 xl:w-20 2xl:w-24`}>{`${time.slice(6, 8)}`}</div>
     </div>
   );
 }
